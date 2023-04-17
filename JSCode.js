611d9041b165118dd597a06b526a7ebe01876fa8
Nassha <92914808+Nassha@users.noreply.github.com>
2023-04-17 18:29:33 +0800

    Update JSCode.js

--- JSCode.js
+++ JSCode.js
@@ -30,11 +30,11 @@ function mintNFT (name,eyecolor,shirtType,bling) {
 // and print their metadata with console.log()
 function listNFTs () {
     for(let i=0; i<NFTs.length;i++){
-        console.log("\nID: "+(i+1));
-        console.log("Name: "+ NFTs[i].name);
-        console.log("Eyecolor: "+ NFTs[i].eyecolor);
+        console.log("\nID: \t\t"+(i+1));
+        console.log("Name: \t\t"+ NFTs[i].name);
+        console.log("Eyecolor: \t"+ NFTs[i].eyecolor);
         console.log("Shirt Type: "+ NFTs[i].shirtType);
-        console.log("Bling: "+ NFTs[i].bling);
+        console.log("Bling: \t\t"+ NFTs[i].bling);
     }
 }
 
